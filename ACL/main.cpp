7cab8ae74de35cf01c2f69b60510bd21d0f21880
Linfeng266 <71446857+Linfeng266@users.noreply.github.com>
2022-01-14 21:14:08 -0500

    Add files via upload

--- ACL/main.cpp
+++ ACL/main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-
+#include "RegisterPage.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -7,5 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    RegisterPage rp;
+    rp.show();
     return a.exec();
+
 }
