f4d3ff8778019b759c67c544bafdcd571a494d12
Pocket99 <leoqiuzirui@gmail.com>
2022-01-25 23:31:50 -0500

    update

--- ACL/RegisterPage.cpp
+++ ACL/RegisterPage.cpp
@@ -6,7 +6,12 @@ RegisterPage::RegisterPage(QWidget *parent) :
     ui(new Ui::RegisterPage)
 {
     ui->setupUi(this);
-
+    db = QSqlDatabase::addDatabase("QMYSQL","MyConnect");
+    db.setHostName("sh-cynosdbmysql-grp-7530641o.sql.tencentcdb.com");//IP address
+    db.setPort(29339);
+    db.setDatabaseName("COEN490");
+    db.setUserName("root");
+    db.setPassword("Coen_490");
 }
 
 RegisterPage::~RegisterPage()
@@ -16,12 +21,6 @@ RegisterPage::~RegisterPage()
 
 void RegisterPage::on_loginBtn_clicked()
 {
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL","MyConnect");
-    db.setHostName("sh-cynosdbmysql-grp-7530641o.sql.tencentcdb.com");//IP address
-    db.setPort(29339);
-    db.setDatabaseName("COEN490");
-    db.setUserName("root");
-    db.setPassword("Coen_490");
 
     QString username = ui->loginUsername->text();
     QString password = ui->loginPassword->text();
@@ -47,7 +46,7 @@ void RegisterPage::on_loginBtn_clicked()
 //                QString usernameFromDB = query.value(1).toString();
 //                QString passwordFromDB = query.value(2).toString();
 //                if(usernameFromDB == username && passwordFromDB == password){
-                    QMessageBox::information(this,"Success","Login Success");
+                    //QMessageBox::information(this,"Success","Login Success");
                     ui->loginUsername->setText("");
                     ui->loginPassword->setText("");
                     hide();
@@ -72,12 +71,7 @@ void RegisterPage::on_loginBtn_clicked()
 
 void RegisterPage::on_registerBtn_clicked()
 {
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("sh-cynosdbmysql-grp-7530641o.sql.tencentcdb.com");//IP address
-    db.setPort(29339);
-    db.setDatabaseName("COEN490");
-    db.setUserName("root");
-    db.setPassword("Coen_490");
+
     QString username = ui->username->text();
     QString password = ui->password->text();
     QString firstname = ui->firstName->text();
@@ -87,54 +81,40 @@ void RegisterPage::on_registerBtn_clicked()
         QMessageBox::information(this,"Register Failed","Please Provide Required Infomation");
     }
     else{
-    if(db.open()){
-        QSqlQuery check;
-        QSqlQuery qry;
-        check.prepare(QString("SELECT * FROM Doctors WHERE username = :username"));
-        check.bindValue(":username",username);
-        if(!check.exec()){
-            QMessageBox::information(this,"Failed","Login Query Failed To Execute");
-        }
-        else {
-        if(check.next()){
-            QMessageBox::information(this,"Register Failed","Username already in used");
-            ui->username->setText("");
-            ui->username->setPlaceholderText("Username");
-            ui->password->setText("");
-            ui->password->setPlaceholderText("Password");
-            ui->firstName->setText("");
-            ui->firstName->setPlaceholderText("First Name");
-            ui->lastName->setText("");
-            ui->lastName->setPlaceholderText("Last Name");
+        if(db.open()){
+            QSqlQuery check;
+            QSqlQuery qry;
+            check.prepare(QString("SELECT * FROM Doctors WHERE username = :username"));
+            check.bindValue(":username",username);
+            if(!check.exec()){
+                QMessageBox::information(this,"Failed","Login Query Failed To Execute");
+            }
+            else {
+                if(check.next()){
+                    QMessageBox::information(this,"Register Failed","Username already in used");
+                    cleanRegUI();
+                }
+                else{
+
+                qry.prepare("INSERT INTO Doctors (username,password,dFirstName,dLastName) VALUES (:username,:password,:firstname,:lastname)");
+                qry.bindValue(":username",username);
+                qry.bindValue(":password",password);
+                qry.bindValue(":firstname",firstname);
+                qry.bindValue(":lastname",lastname);
+
+                    if(qry.exec()){
+                        QMessageBox::information(this,"Inserted","Register Success");
+                        cleanRegUI();
+
+                    }else {
+                        QMessageBox::information(this,"Not Inserted","Data Inserted failed");
+                    }
+                }
+            }
         }
         else{
-
-        qry.prepare("INSERT INTO Doctors (username,password,dFirstName,dLastName) VALUES (:username,:password,:firstname,:lastname)");
-        qry.bindValue(":username",username);
-        qry.bindValue(":password",password);
-        qry.bindValue(":firstname",firstname);
-        qry.bindValue(":lastname",lastname);
-
-        if(qry.exec()){
-            QMessageBox::information(this,"Inserted","Register Success");
-            ui->username->setText("");
-            ui->username->setPlaceholderText("Username");
-            ui->password->setText("");
-            ui->password->setPlaceholderText("Password");
-            ui->firstName->setText("");
-            ui->firstName->setPlaceholderText("First Name");
-            ui->lastName->setText("");
-            ui->lastName->setPlaceholderText("Last Name");
-
-        }else {
-            QMessageBox::information(this,"Not Inserted","Data Inserted failed");
+            QMessageBox::information(this,"Not Connected","Database Connected fail");
         }
-    }
-    }
-    }
-    else{
-        QMessageBox::information(this,"Not Connected","Database Connected fail");
-    }
     }
 }
 
@@ -144,6 +124,16 @@ void RegisterPage::on_label_4_linkActivated(const QString &link)
 
 }
 
-
+void RegisterPage::cleanRegUI()
+{
+    ui->username->setText("");
+    ui->username->setPlaceholderText("Username");
+    ui->password->setText("");
+    ui->password->setPlaceholderText("Password");
+    ui->firstName->setText("");
+    ui->firstName->setPlaceholderText("First Name");
+    ui->lastName->setText("");
+    ui->lastName->setPlaceholderText("Last Name");
+}
 
 
